f574c12f9ee36ec9a4803c4916f57c1060f75322
DeeChael <DeeChael@outlook.com>
2024-03-02 21:23:45 +0800

    fix: forget upload & no class def error on velocity platform

--- library-multiplatform/src/main/java/fit/d6/candy/api/LibraryProvider.java
+++ library-multiplatform/src/main/java/fit/d6/candy/api/LibraryProvider.java
@@ -1,7 +1,5 @@
 package fit.d6.candy.api;
 
-import org.bukkit.Bukkit;
-
 import java.lang.reflect.InvocationTargetException;
 
 class LibraryProvider {
@@ -11,8 +9,8 @@ class LibraryProvider {
     static CandyLibrary search() {
         if (library == null) {
             try {
-                library = (CandyLibrary) Bukkit.getPluginManager().getPlugin("CandyLibrary");
-            } catch (Exception e) {
+                library = (CandyLibrary) org.bukkit.Bukkit.getPluginManager().getPlugin("CandyLibrary");
+            } catch (NoClassDefFoundError e) {
                 try {
                     library = (CandyLibrary) Class.forName("fit.d6.candy.velocity.CandyLibraryPlugin").getDeclaredMethod("getInstance").invoke(null);
                 } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException |
