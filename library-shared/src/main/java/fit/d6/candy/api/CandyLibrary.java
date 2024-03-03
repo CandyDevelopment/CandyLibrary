8703913f4cceee100fda354a2edbb04474d62a10
DeeChael <DeeChael@outlook.com>
2024-03-03 10:16:33 +0800

    fix: missing classes and methods in shared module

--- library-shared/src/main/java/fit/d6/candy/api/CandyLibrary.java
+++ library-shared/src/main/java/fit/d6/candy/api/CandyLibrary.java
@@ -6,8 +6,18 @@ public interface CandyLibrary {
 
     @NotNull <S extends Service> S getService(@NotNull Class<S> clazz);
 
+    <S extends Service> boolean isServiceConfigured(@NotNull Class<S> clazz);
+
+    @NotNull
+    CandyVersion getVersion();
+
     @NotNull
     static CandyLibrary getLibrary() {
+        throw new RuntimeException();
+    }
+
+    @NotNull
+    static CandyVersion version() {
         throw new RuntimeException();
     }
 
---
+++ library-shared/src/main/java/fit/d6/candy/api/CandyVersion.java
@@ -0,0 +1,38 @@
+package fit.d6.candy.api;
+
+public enum CandyVersion {
+
+    V1_17_R1,
+    V1_18_R1,
+    V1_18_R2,
+    V1_19_R1,
+    V1_19_R2,
+    V1_19_R3(true, true),
+    V1_20_R1(true, true),
+    V1_20_R2(true, true),
+    V1_20_R3(true, true),
+    V1_21(true, true),
+    PROXY(false, false);
+
+
+    private final boolean foliaSchedulerSupport;
+    private final boolean worldEnvironemtnSupport;
+
+    CandyVersion() {
+        this(false, false);
+    }
+
+    CandyVersion(boolean foliaSchedulerSupport, boolean worldEnvironmentSupport) {
+        this.foliaSchedulerSupport = foliaSchedulerSupport;
+        this.worldEnvironemtnSupport = worldEnvironmentSupport;
+    }
+
+    public boolean isFoliaSchedulerSupport() {
+        return foliaSchedulerSupport;
+    }
+
+    public boolean isWorldEnvironemtnSupport() {
+        return worldEnvironemtnSupport;
+    }
+
+}
